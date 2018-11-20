436a754a885307550f1bb84a7efa8d8fa8c396b5
Sean Yen <seanyen@microsoft.com>
2018-11-20 06:09:45 +0000

    Added file setup.bat

---
+++ setup.bat
@@ -0,0 +1,5 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+set PATH=%PATH:C:\tools\mingw64\bin;=%
+set PATH=%PATH:C:\Program Files\Git\bin;=%
+set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
+set PATH=%PATH:C:\Program Files\Git\mingw64\bin;=%
