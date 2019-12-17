d8c503fbad2e927e32ecf9b14afb5a842bf4df60
seanyen <seanyen@microsoft.com>
2019-12-17 11:50:34 -0800

    fix.

--- setup.bat
+++ setup.bat
@@ -1,5 +1,6 @@
+:: Use Visual Studio 2017
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-set PATH=%PATH:C:\tools\mingw64\bin;=%
-set PATH=%PATH:C:\Program Files\Git\bin;=%
-set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
-set PATH=%PATH:C:\Program Files\Git\mingw64\bin;=%
+
+:: MSVC is preferred.
+set CC=cl.exe
+set CXX=cl.exe
