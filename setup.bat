ff6730969840274ae86b35a460dda0c37663fb46
Sean Yen <seanyen@microsoft.com>
2019-12-17 16:19:46 -0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
-:: Use Visual Studio 2017
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+:: Use Visual Studio 2019
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 
 :: MSVC is preferred.
 set CC=cl.exe
